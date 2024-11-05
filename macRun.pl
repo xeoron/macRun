fd834b82b81c5d252d2adc93567c616137e2152b
Jason Campisi <aitsinformation@gmail.com>
2024-11-05 10:18:58 -0500

    v1.0.1
    Reworked phrasings

--- macRun.pl
+++ macRun.pl
@@ -2,7 +2,7 @@
 # Name: macRun.pl
 # Author: Jason Campisi
 # Date: 11/2/2024
-# Version 1.0.0
+# Version 1.0.1
 # Purpose: This releases the macOS gatekeeper flag on a program downloaded from the internet that is not a signed-app.
 #          Not unsigned apps on MacOS if compiled in a system will run fine, but it's downloaded from the Net then 
 #          Gatekeeper gives the app a “quarantined” flag that is was downloaded from a arbitrary websites. 
@@ -20,9 +20,11 @@ if ($program ne ""){
     qx\xattr -d com.apple.quarantine $program\;
 } else {
  print <<EOD;
-macRun releases the macOS gatekeeper flag on a program downloaded from the internet that is not a signed-app.
-Not unsigned apps on MacOS if compiled in a system will run fine, but it's downloaded from the Net then 
-Gatekeeper gives the app a “quarantined” flag that is was downloaded from a arbitrary websites. 
+This releases the macOS gatekeeper flag on a program downloaded from the internet that is not a signed-app.
+This is not the case for unsigned apps on MacOS if compiled in the system built or a custom script-- 
+they will will run fine. It's when downloaded from the Net that activates Gatekeeper.  
+Gatekeeper gives a download app a “quarantined” flag that is was downloaded from a arbitrary websites 
+and if unsigned takes more action.
 
 Usage:  $name -u program_to_unflag
 EOD
